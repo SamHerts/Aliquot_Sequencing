9ff715412a35db5c43d534d81a8b94e989269b52
SamHerts <sdherts@gmail.com>
2024-05-06 10:26:51 -0400

    move algorithm helper functions to tile implementation

--- src/sieve/algorithm/tile.rs
+++ src/sieve/algorithm/tile.rs
@@ -1,6 +1,6 @@
-//! Multi-threaded sieving of tiles.
+//! Multithreaded sieving of tiles.
 
-use super::{calculate_batch_size, calculate_block_offset, Algorithm};
+use super::{ Algorithm};
 use crate::sieve::{FlagDataExecute, Sieve, SieveExecute};
 use crate::DataType;
 
@@ -10,7 +10,7 @@ use rayon::prelude::*;
 ///
 /// This algorithm works by first finding all primes up to the square root of the sieve size (and
 /// sieving that part of the sieve) with a single thread. Then, the rest of the sieve is split into
-/// work units capped by the provided working set size. For each of this units, one thread sieves
+/// work units capped by the provided working set size. For each of these units, one thread sieves
 /// all found primes.
 ///
 /// This algorithm has some interesting properties. At infinite cores, its performance is mostly
@@ -112,4 +112,33 @@ fn get_primes<F: FlagDataExecute<D>, D: DataType>(
     }
 
     primes
+}
+
+/// Helper method that returns the amount of elements each chunk should contain.
+#[inline]
+fn calculate_batch_size<D: DataType>(data_len: usize, max_size: usize) -> usize {
+    let elements_per_line = (64 * 8 / D::BITS).max(1);
+
+    let thread_align = (data_len + rayon::current_num_threads() - 1) / rayon::current_num_threads();
+    let cache_line_align = (thread_align + elements_per_line - 1) & !(elements_per_line - 1);
+    cache_line_align.max(elements_per_line).min(max_size)
+}
+
+/// Calculates the start offset for a sieve pass for a block that is offset to the start of the
+/// sieve.
+#[inline]
+fn calculate_block_offset(start_index: usize, offset: usize, prime: usize) -> usize {
+    if offset <= start_index {
+        start_index - offset
+    } else {
+        // This tells us how far offset is beyond the last flag to be reset before
+        // it. Translating between flag indices and actual numbers makes this
+        // formula look a bit more complicated than it is.
+        let reset_offset = (offset + prime / 2 + 1) % prime;
+        if reset_offset == 0 {
+            0
+        } else {
+            prime - reset_offset
+        }
+    }
 }
