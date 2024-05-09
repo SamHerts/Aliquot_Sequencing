ded7fcd0618a7069fafac237a5a908f201ef10d0
Samuel Herts <hertssd@ornl.gov>
2024-05-09 08:25:25 -0400

    Add more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,73 @@
 extern crate num;
 
-
 use std::collections::HashMap;
 use prime_factorization::Factorization;
 use num::{BigUint, FromPrimitive, Integer, One, Zero};
+use std::fmt::{Display, Formatter, Error, Debug};
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct PrimePower {
+    prime_factor: BigUint,
+    power_count: usize,
+}
+
+struct FactorVec(Vec<PrimePower>);
+
+impl Display for FactorVec {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        for v in &self.0 {
+            write!(f, "{} * ", v)?;
+        }
+        Ok(())
+    }
+}
+
+// impl Debug for PrimePower {
+//     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+//         write!(f, "{}*{}", self.prime_factor, self.power_count)
+//     }
+// }
+
+impl Display for PrimePower {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}*{}", self.prime_factor, self.power_count)
+    }
+}
+
+impl From<PrimePower> for (BigUint, usize) {
+    fn from(e: PrimePower) -> (BigUint, usize) {
+        let PrimePower { prime_factor, power_count }: PrimePower = e;
+        (prime_factor, power_count)
+    }
+}
+
+impl From<(BigUint, usize)> for PrimePower {
+    fn from(e: (BigUint, usize)) -> PrimePower {
+        let (a, b): (BigUint, usize) = e;
+        PrimePower{ prime_factor: a, power_count: b }
+    }
+}
 
 
 fn main() {
-    // let mut num: u128 = 276;
-    let mut num: u128 = 169766367014411422183878825491823897888;
-    let mut step = 0;
+    let mut num: u128 = 276;
+    // let mut num: u128 = 169766367014411422183878825491823897888;
+    let mut step = 1;
     let mut too_big_flag = false;
     let mut temp_num: BigUint = Default::default();
     let mut division: BigUint = Default::default();
     let mut remainder: BigUint = Default::default();
 
     loop {
+        let mut two_count = 0;
         if too_big_flag {
-            (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Nope"));
-            let mut two_count = 0;
+            (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Can't divide by 2"));
             while remainder == Zero::zero() {
                 two_count += 1;
-                (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Nope"));
+                (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Can't divide by 2 again"));
+                if remainder == Zero::zero() {
+                    temp_num = division;
+                }
             }
             println!("two count: {}", two_count);
             let next_num = u128::try_from(temp_num.clone());
@@ -30,13 +76,15 @@ fn main() {
                     num = n;
 
                     },
-                Err(..) => {println!("Nope"); break;},
+                Err(..) => {println!("Can't convert to int"); break;},
             }
         }
         
         let mut factors = Factorization::run(num).factors;
-        if too_big_flag {
-            factors.push(2);
+        if too_big_flag && two_count > 0 {
+            for _ in 1..two_count {
+                factors.push(2);
+            }
         }
         // Create a HashMap to count occurrences of each integer
         let mut counts: HashMap<BigUint, usize> = HashMap::new();
@@ -45,19 +93,18 @@ fn main() {
         }
 
         // Create a new vector with counts
-        let deduplicated_vector: Vec<(BigUint, usize)> = counts.into_iter().collect();
+        let mut deduplicated_vector: Vec<PrimePower> = counts.into_iter().map(|(key, value)| PrimePower { prime_factor: key, power_count: value }).collect();
+        deduplicated_vector.sort();
 
         let mut result: BigUint = One::one();
 
-        for (val, power) in &deduplicated_vector {
-            let temp: BigUint = (val.pow((*power + 1) as u32) - BigUint::one()) / (val - BigUint::one());
+        for prime in &deduplicated_vector  {
+            let temp: BigUint = (prime.prime_factor.pow(((prime.power_count) + 1) as u32) - BigUint::one()) / (prime.prime_factor.clone() - BigUint::one());
             result *= temp;
         }
         result -= num.clone();
-        println!("{result}");
 
-
-        println!("Step: {}, Factors: {:?}, Sum: {}, Number: {}", step, deduplicated_vector, result, num);
+        println!("{} .  {} = {}", step, result, deduplicated_vector);
         if result == Zero::zero() {
             break;
         }
