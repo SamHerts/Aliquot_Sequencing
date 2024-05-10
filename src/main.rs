4c0ddbd68bf0be83d287e17a60bb563e80c92802
Samuel Herts <hertssd@ornl.gov>
2024-05-10 11:57:18 -0400

    Update to remove factors of 3.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate num;
 
 use std::collections::HashMap;
 use prime_factorization::Factorization;
-use num::{BigUint, FromPrimitive, Integer, One, Zero};
+use num::{BigUint, FromPrimitive, Integer, One, ToPrimitive, Zero};
 use std::fmt::{Display, Formatter, Error, Debug};
+use std::ops::{Div, Sub};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 struct PrimePower {
@@ -22,11 +23,11 @@ impl Display for FactorVec {
     }
 }
 
-// impl Debug for PrimePower {
-//     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-//         write!(f, "{}*{}", self.prime_factor, self.power_count)
-//     }
-// }
+impl Debug for PrimePower {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}*{}", self.prime_factor, self.power_count)
+    }
+}
 
 impl Display for PrimePower {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
@@ -48,39 +49,76 @@ impl From<(BigUint, usize)> for PrimePower {
     }
 }
 
+fn remove_factor(mut composite: BigUint, factor_to_remove: u32) -> (BigUint, usize) {
+    let mut factor_count: usize = 1;
+    let mut div = composite.clone();
+    let mut rem;
+    while composite.is_multiple_of(&BigUint::from(factor_to_remove)) {
+        factor_count += 1;
+        (div, rem) = composite.div_rem(&BigUint::from(factor_to_remove));
+        println!("{composite} / {factor_to_remove} = {div}");
+        if rem == Zero::zero() {
+            composite = div.clone();
+        }
+        else {
+            break;
+        }
+    }
+    (div, factor_count)
+}
+
 
 fn main() {
-    let mut num: u128 = 276;
-    // let mut num: u128 = 169766367014411422183878825491823897888;
+    // let mut num: u128 = 276;
+    let mut num: u128 = 169766367014411422183878825491823897888;
     let mut step = 1;
     let mut too_big_flag = false;
     let mut temp_num: BigUint = Default::default();
+    let mut working_num: BigUint = Default::default();
     let mut division: BigUint = Default::default();
     let mut remainder: BigUint = Default::default();
 
     loop {
-        let mut two_count = 0;
+        let mut two_count = 1;
         if too_big_flag {
-            (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Can't divide by 2"));
-            while remainder == Zero::zero() {
-                two_count += 1;
-                (division, remainder) = temp_num.div_rem(&BigUint::from_u32(2).expect("Can't divide by 2 again"));
-                if remainder == Zero::zero() {
-                    temp_num = division;
-                }
-            }
-            println!("two count: {}", two_count);
-            let next_num = u128::try_from(temp_num.clone());
-            match next_num {
-                Ok(n)=> {
-                    num = n;
+            println!("Beginnging of Loop: {working_num}");
+            println!("Could not convert {working_num} to u128. Removing powers of 2");
+            temp_num = working_num.clone();
+            // while temp_num.is_multiple_of(&BigUint::from(2u8)) {
+            //     two_count += 1;
+            //     (division, remainder) = temp_num.div_rem(&BigUint::from(2u8));
+            //     if remainder == Zero::zero() {
+            //         println!("{temp_num} / 2 = {division}");
+            //         temp_num = division;
+            //     }
+            // }
 
-                    },
-                Err(..) => {println!("Can't convert to int"); break;},
+            (temp_num, two_count) = remove_factor(temp_num, 2);
+
+            let next_num = temp_num.to_u128();
+            match next_num {
+                Some(n)=> { num = n; },
+                None => {
+                    println!("Could not convert {temp_num} to u128. Removing powers of 3");
+                    (temp_num, two_count) = remove_factor(temp_num, 3);
+                    let next_num = temp_num.to_u128();
+                    match next_num {
+                        Some(n)=> { num = n; },
+                        None => {
+                            println!("Could not convert {temp_num} to u128 even after removing powers of 3");
+                            break;
+                        },
+                    }
+                },
             }
         }
-        
+        else {
+            println!("Beginnging of Loop: {num}");
+        }
+
+        println!("Trying to factor {num}");
         let mut factors = Factorization::run(num).factors;
+        // !TODO: Add factors of 3 back into the vector
         if too_big_flag && two_count > 0 {
             for _ in 1..two_count {
                 factors.push(2);
@@ -99,21 +137,35 @@ fn main() {
         let mut result: BigUint = One::one();
 
         for prime in &deduplicated_vector  {
-            let temp: BigUint = (prime.prime_factor.pow(((prime.power_count) + 1) as u32) - BigUint::one()) / (prime.prime_factor.clone() - BigUint::one());
+            // (p^(a+1) - 1) / (p - 1)
+            // let temp: BigUint = (prime.prime_factor.pow(((prime.power_count) + 1) as u32) - BigUint::one()) / (prime.prime_factor.clone() - BigUint::one());
+            let pa1 = prime.prime_factor.pow(((prime.power_count) + 1) as u32);
+            let pa1minus1 = pa1.clone() - BigUint::one();
+            let pminus1 = prime.prime_factor.clone() - BigUint::one();
+            let temp = pa1minus1.clone().div(pminus1.clone());
+            println!("Working on {prime}, p^(a+1) = {pa1} | p^(a+1) - 1 = {pa1minus1} | p - 1 = {pminus1} | p^(a+1) / (p-1) = {temp}");
             result *= temp;
         }
-        result -= num.clone();
 
-        println!("{} .  {} = {}", step, result, deduplicated_vector);
+        if too_big_flag{
+            result -= working_num.clone();
+            println!("{step} .  {working_num} = {deduplicated_vector:?} | Sum of Divisors: {result}");
+        }
+        else {
+            result -= num;
+            println!("{step} .  {num} = {deduplicated_vector:?} | Sum of Divisors: {result}");
+        }
+
+
         if result == Zero::zero() {
             break;
         }
         step += 1;
 
-        let next_num = u128::try_from(result.clone());
+        let next_num = result.to_u128();
         match next_num {
-            Ok(n)=> num = n,
-            Err(..) => {too_big_flag = true; temp_num = result.clone()},
+            Some(n)=> {num = n; too_big_flag = false},
+            None => {too_big_flag = true; working_num = result.clone()},
         }
     }
 }
